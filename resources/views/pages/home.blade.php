e469977ace587d434ad4e5ee51beb20cb95dc60e
Quang Huy <62685126+qhuy1901@users.noreply.github.com>
2021-10-03 16:57:37 +0700

    commit

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,7 +7,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<!-- Title Tag  -->
-	<title>ITGo Shop</title>
+	<title>TạQuang Huy</title>
 	<!-- Favicon -->
 	<link rel="icon" href="{{('public/frontend/images/favi.png')}}">
 	 <!-- Fonts-->
