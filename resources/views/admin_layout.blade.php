4058b2a813bb7943da1edc1b2c2adee29e50ccd3
Lê Thị Hồng Cúc <cuclth2701@gmail.com>
2021-10-04 10:12:17 +0700

    add logo

--- resources/views/admin_layout.blade.php
+++ resources/views/admin_layout.blade.php
@@ -1,11 +1,11 @@
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="vi">
 <head>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<title>Trang Admin </title>
 	<meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
-	<link rel="icon" href="{{asset('./public/admin/img/icon.ico')}}" type="image/x-icon"/>
+	<link rel="icon" type="image/png" href="{{('./public/admin/images/favi.png')}}" />
 
 	<!-- Fonts and icons -->
 	<script src="{{asset('./public/admin/js/plugin/webfont/webfont.min.js')}}"></script>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,9 +7,9 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<!-- Title Tag  -->
-	<title>TạQuang Huy</title>
+	<title>ITGo Shop</title>
 	<!-- Favicon -->
-	<link rel="icon" href="{{('public/frontend/images/favi.png')}}">
+	<link rel="icon" href="{{asset('public/frontend/images/favi.png')}}">
 	 <!-- Fonts-->
 	<link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">	<!-- StyleSheet -->
 	 <link rel="stylesheet" href="{{('public/frontend/plugins/font-awesome/css/font-awesome.min.css')}}">
