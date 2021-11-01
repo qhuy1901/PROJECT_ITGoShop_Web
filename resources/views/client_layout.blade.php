4aa15de861f79e9dc35b98a2617082891513186f
Quang Huy <62685126+qhuy1901@users.noreply.github.com>
2021-11-01 13:17:49 +0700

    Update client_layout.blade.php

--- resources/views/client_layout.blade.php
+++ resources/views/client_layout.blade.php
@@ -331,8 +331,8 @@
 	</footer>
 	<!-- /End Footer Area -->
  
-	<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
-	<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<!-- <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+	<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script> -->
 	<script  type="text/javascript">
 		function removeItem(id)
 		{
