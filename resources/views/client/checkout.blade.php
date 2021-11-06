515a92b61b93bdc4ced372f66395838b1cc76261
Quang Huy <62685126+qhuy1901@users.noreply.github.com>
2021-11-06 15:30:59 +0700

    add some code

--- resources/views/client/checkout.blade.php
+++ resources/views/client/checkout.blade.php
@@ -1,5 +1,19 @@
 @extends('client_layout')
 @section('client_content')
+<div class="breadcrumbs">
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<div class="bread-inner">
+							<ul class="bread-list">
+								<li><a href="index.html">Home<i class="ti-arrow-right"></i></a></li>
+								<li class="active"><a href="blog-single.html">Checkout</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 <section>
 			<div class="container">
 				<div class ="row" style="margin: 20px 0px 0px 0px;">
---
+++ resources/views/client/shipping-address.blade.php
@@ -0,0 +1,8 @@
+@extends('client_layout')
+@section('client_content')
+<section>
+    <div>
+        Hello
+    </div>
+<section>
+@endsection
