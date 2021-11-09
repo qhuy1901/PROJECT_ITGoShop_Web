e3057b4797e9a5dfdc6eccf3448e12fb9dcb3a0e
Lê Thị Hồng Cúc <cuclth2701@gmail.com>
2021-11-09 12:24:12 +0700

    commit

--- resources/views/client/product-listing.blade.php
+++ resources/views/client/product-listing.blade.php
@@ -44,16 +44,15 @@
       </section> 
       <div class="ps-products-wrap pb-80">
         <div class="ps-products" data-mh="product-listing">
-                                <div class="ps-product__columns">
+          <div class="ps-product__columns">
                                   <div class="ps-product__column">
-                                    <div class="ps-shoe mb-30">
+                                    <div class="ps-shoe mb-20">
                                       <div class="ps-shoe__thumbnail">
                                         <div class="ps-badge"><span>New</span></div>
-                                        <div class="ps-badge ps-badge--sale ps-badge--2nd"><span>-35%</span></div><a class="ps-shoe__favorite" href="#"><i class="ps-icon-heart"></i></a><img src="{{url('./public/client/Images/shoe/2.jpg')}}" alt=""><a class="ps-shoe__overlay" href="product-detail.html"></a>
+                                        <div class="ps-badge ps-badge--sale ps-badge--2nd"><span>-35%</span></div><a class="ps-shoe__favorite" href="#"><i class="fa fa-heart-o" style="font-size: 15px"></i></a><img src="{{url('./public/client/Images/shoe/2.jpg')}}" alt=""><a class="ps-shoe__overlay" href="product-detail.html"></a>
                                       </div>
                                       <div class="ps-shoe__content">
                                         <div class="ps-shoe__variants">
-                                          <div class="ps-shoe__variant normal"><img src="{{url('./public/client/Images/shoe/2.jpg')}}" alt=""><img src="{{url('./public/client/Images/shoe/2.jpg')}}" alt=""></div>
                                           <select class="ps-rating ps-shoe__rating">
                                             <option value="1">1</option>
                                             <option value="1">2</option>
@@ -77,7 +76,7 @@
                                       </div>
                                       <div class="ps-shoe__content">
                                         <div class="ps-shoe__variants">
-                                          <div class="ps-shoe__variant normal"><img src="{{url('./public/client/Images/shoe/2.jpg')}}" alt=""><img src="{{url('./public/client/Images/shoe/2.jpg')}}" alt=""></div>
+                                        <div class="ps-shoe__variant normal"><img src="{{url('./public/client/Images/shoe/2.jpg')}}" alt=""><img src="{{url('./public/client/Images/shoe/3.jpg')}}" alt=""><img src="{{url('./public/client/Images/shoe/4.jpg')}}" alt=""><img src="{{url('./public/client/Images/shoe/5.jpg')}}" alt=""></div>
                                           <select class="ps-rating ps-shoe__rating">
                                             <option value="1">1</option>
                                             <option value="1">2</option>
@@ -101,31 +100,7 @@
                                       </div>
                                       <div class="ps-shoe__content">
                                         <div class="ps-shoe__variants">
-                                          <div class="ps-shoe__variant normal"><img src="{{url('./public/client/Images/shoe/2.jpg')}}" alt=""><img src="{{url('./public/client/Images/shoe/2.jpg')}}" alt=""></div>
-                                          <select class="ps-rating ps-shoe__rating">
-                                            <option value="1">1</option>
-                                            <option value="1">2</option>
-                                            <option value="1">3</option>
-                                            <option value="1">4</option>
-                                            <option value="2">5</option>
-                                          </select>
-                                        </div>
-                                        <div class="ps-shoe__detail"><a class="ps-shoe__name" href="#">Air Jordan 7 Retro</a>
-                                          <p class="ps-shoe__categories"><a href="#">Men shoes</a>,<a href="#"> Nike</a>,<a href="#"> Jordan</a></p><span class="ps-shoe__price">
-                                            <del>£220</del> £ 120</span>
-                                        </div>
-                                      </div>
-                                    </div>
-                                  </div>
-                                  <div class="ps-product__column">
-                                    <div class="ps-shoe mb-30">
-                                      <div class="ps-shoe__thumbnail">
-                                        <div class="ps-badge"><span>New</span></div>
-                                        <div class="ps-badge ps-badge--sale ps-badge--2nd"><span>-35%</span></div><a class="ps-shoe__favorite" href="#"><i class="ps-icon-heart"></i></a><img src="{{url('./public/client/Images/shoe/2.jpg')}}" alt=""><a class="ps-shoe__overlay" href="product-detail.html"></a>
-                                      </div>
-                                      <div class="ps-shoe__content">
-                                        <div class="ps-shoe__variants">
-                                          <div class="ps-shoe__variant normal"><img src="{{url('./public/client/Images/shoe/2.jpg')}}" alt=""><img src="{{url('./public/client/Images/shoe/2.jpg')}}" alt=""></div>
+                                        <div class="ps-shoe__variant normal"><img src="{{url('./public/client/Images/shoe/2.jpg')}}" alt=""><img src="{{url('./public/client/Images/shoe/3.jpg')}}" alt=""><img src="{{url('./public/client/Images/shoe/4.jpg')}}" alt=""><img src="{{url('./public/client/Images/shoe/5.jpg')}}" alt=""></div>
                                           <select class="ps-rating ps-shoe__rating">
                                             <option value="1">1</option>
                                             <option value="1">2</option>
