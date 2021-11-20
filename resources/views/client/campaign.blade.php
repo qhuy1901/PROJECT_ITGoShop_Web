ba5386c8dac59c7e2bac83736283e4cc09db4247
Lê Thị Hồng Cúc <cuclth2701@gmail.com>
2021-11-20 13:26:59 +0700

    commit

--- resources/views/client/campaign.blade.php
+++ resources/views/client/campaign.blade.php
@@ -5,25 +5,7 @@
 <section class="hero-slider">
 		<!-- Single Slider -->
 		<div class="single-slider">
-			<div class="container">
-				<div class="row no-gutters">
-					<div class="col-lg-9 offset-lg-3 col-12">
-						<div class="text-inner">
-							<div class="row">
-								<div class="col-lg-7 col-12">
-									<div class="hero-text">
-										<h1><span>DEAL HOT </span>Macbook Air </h1>
-										<p>Giá chỉ từ : 26.490.000 ₫</p>
-										<div class="button">
-											<a href="ProductDetail.html" class="btn">Shop Now!</a>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+			<img src="./Images/pannel/pn.png">
 		</div>
 		<!--/ End Single Slider -->
 </section>
